fa06b124470e0687fad8b47da4fbcf851685a2dd
Brittany <Birittany@DCC-M1-2.home>
2026-08-21 15:43:45 -0500

    Fix remaining legacy Pages Liquid filters

--- assets/js/lunr-store.js
+++ assets/js/lunr-store.js
@@ -1,18 +1,26 @@
 ---
 # create lunr store for search page
 ---
-{% if site.data.theme.search-child-objects == true %}
-{%- assign items = site.data[site.metadata] | where_exp: 'item','item.objectid' -%}
-{% else %}
-{%- assign items = site.data[site.metadata] | where_exp: 'item','item.objectid and item.parentid == nil' -%}
-{% endif %}
 {%- assign fields = site.data.config-search -%}
 var store = [ 
-{%- for item in items -%} 
+{%- assign first_store_item = true -%}
+{%- for item in site.data[site.metadata] -%}
+{%- if item.objectid -%}
+{%- assign include_search_item = false -%}
+{%- if site.data.theme.search-child-objects == true -%}
+{%- assign include_search_item = true -%}
+{%- elsif item.parentid == nil or item.parentid == '' -%}
+{%- assign include_search_item = true -%}
+{%- endif -%}
+{%- if include_search_item -%}
+{%- unless first_store_item -%},{%- endunless -%}
 {  
 {% for f in fields %}{% if item[f.field] %}{{ f.field | jsonify }}: {{ item[f.field] | normalize_whitespace | replace: '""','"' | jsonify }},{% endif %}{% endfor %} 
 "id": {% if item.parentid %}{{ item.parentid | append: '.html#' | append: item.objectid | jsonify }}{% else %}{{item.objectid | append: '.html' | jsonify }}{% endif %}
 
-}{%- unless forloop.last -%},{%- endunless -%}
+}
+{%- assign first_store_item = false -%}
+{%- endif -%}
+{%- endif -%}
 {%- endfor -%}
 ];
